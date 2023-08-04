01cbcf5c1e87ad5f2bd99aaee38544d3952238e2
sui4 <sui4eau@gmail.com>
2023-08-05 00:25:28 +0900

    指定したオブジェクトの子のレンダラーをMaterialGroupに追加するMonobehaviour

---
+++ Runtime/ChildRenderersAdderToMaterialGroup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace sui4.MaterialPropertyBaker
+{
+    public class ChildRenderersAdderToMaterialGroup : MonoBehaviour
+    {
+        [SerializeField] private MaterialGroup _targetMaterialGroup;
+        [SerializeField] private GameObject _targetParent;
+
+        private void OnValidate()
+        {
+            if (_targetMaterialGroup == null || _targetParent == null) return;
+
+            var renderers = _targetParent.GetComponentsInChildren<Renderer>(true);
+            foreach (var renderer in renderers)
+            {
+                if (renderer == null) continue;
+                if (_targetMaterialGroup.Renderers.Contains(renderer)) continue;
+                _targetMaterialGroup.Renderers.Add(renderer);
+            }
+
+            _targetMaterialGroup.OnValidate();
+        }
+    }
+}
