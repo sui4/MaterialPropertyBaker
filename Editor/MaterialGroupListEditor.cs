4fe66be8192afb9982e290507de5c9519b4b404c
sui4 <sui4eau@gmail.com>
2023-08-08 16:24:01 +0900

    Add Editors

---
+++ Editor/MaterialGroupListEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace sui4.MaterialPropertyBaker
+{
+    [CustomEditor(typeof(MaterialGroupList))]
+    public class MaterialGroupListEditor : Editor
+    {
+        private List<MaterialGroup> _materialGroupList = new List<MaterialGroup>();
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
