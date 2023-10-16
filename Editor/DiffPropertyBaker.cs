27a3f5230743f15c87802d53c6c1675038571ce5
sui4 <sui4eau@gmail.com>
2023-10-16 23:25:13 +0900

    Switchのcaseを追加

--- Editor/DiffPropertyBaker.cs
+++ Editor/DiffPropertyBaker.cs
@@ -141,6 +141,8 @@ namespace sui4.MaterialPropertyBaker
                             EditorGUILayout.IntField(new GUIContent(prop.PropName), prop.BaseIntValue);
                             EditorGUILayout.IntField(new GUIContent(prop.PropName), prop.TargetIntValue);
                             break;
+                        case ShaderPropertyType.Vector:
+                        case ShaderPropertyType.Texture:
                         default:
                             Debug.LogWarning(
                                 $"Property type {prop.PropType} is not supported. Skipped. (This should not happen))");
@@ -191,6 +193,8 @@ namespace sui4.MaterialPropertyBaker
                             case ShaderPropertyType.Int:
                                 targetMatProps.SetInt(prop.PropName, prop.TargetIntValue);
                                 break;
+                            case ShaderPropertyType.Vector:
+                            case ShaderPropertyType.Texture:
                             default:
                                 Debug.LogWarning(
                                     $"Property type {prop.PropType} is not supported. Skipped. (This should not happen))");
